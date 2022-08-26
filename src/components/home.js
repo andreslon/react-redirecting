8f55ed624ea7229a9b02bd3a6b52f6460ec0ae63
Andrés Londoño <andreslon@outlook.com>
2022-08-26 00:55:14 -0500

    improve design

---
+++ src/components/home.js
@@ -0,0 +1,155 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { styled, ThemeProvider, createTheme } from "@mui/material/styles";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import ArrowRight from "@mui/icons-material/ArrowRight";
+import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
+import Home from "@mui/icons-material/Home";
+import Settings from "@mui/icons-material/Settings";
+import People from "@mui/icons-material/People";
+import PermMedia from "@mui/icons-material/PermMedia";
+import Dns from "@mui/icons-material/Dns";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+
+import Public from "@mui/icons-material/Public";
+import background from "./../bg.png";
+
+const data = [
+  { icon: <People />, label: "Authentication" },
+  { icon: <Dns />, label: "Database" },
+  { icon: <PermMedia />, label: "Storage" },
+  { icon: <Public />, label: "Hosting" },
+];
+
+const FireNav = styled(List)({
+  "& .MuiListItemButton-root": {
+    paddingLeft: 24,
+    paddingRight: 24,
+  },
+  "& .MuiListItemIcon-root": {
+    minWidth: 0,
+    marginRight: 16,
+  },
+  "& .MuiSvgIcon-root": {
+    fontSize: 20,
+  },
+});
+
+export default function CustomizedList() {
+  const [open, setOpen] = React.useState(true);
+  return (
+    <Box sx={{ display: "flex" }}>
+      <ThemeProvider
+        theme={createTheme({
+          components: {
+            MuiListItemButton: {
+              defaultProps: {
+                disableTouchRipple: true,
+              },
+            },
+          },
+          palette: {},
+        })}
+      >
+        <Paper elevation={0} sx={{ width: "80%" }}>
+          <FireNav component="nav" disablePadding>
+            <img src={background} className="App-bg" alt="Legendary Player" />
+            <Divider />
+            <ListItem component="div" disablePadding>
+              <ListItemButton sx={{ height: 56 }}>
+                <ListItemIcon>
+                  <Home color="primary" />
+                </ListItemIcon>
+                <ListItemText
+                  primary="Project Overview"
+                  primaryTypographyProps={{
+                    color: "primary",
+                    fontWeight: "medium",
+                    variant: "body2",
+                  }}
+                />
+              </ListItemButton>
+              <Tooltip title="Project Settings">
+                <IconButton
+                  size="large"
+                  sx={{
+                    "& svg": {
+                      color: "rgba(255,255,255,0.8)",
+                      transition: "0.2s",
+                      transform: "translateX(0) rotate(0)",
+                    },
+                    "&:hover, &:focus": {
+                      bgcolor: "unset",
+                      "& svg:first-of-type": {
+                        transform: "translateX(-4px) rotate(-20deg)",
+                      },
+                      "& svg:last-of-type": {
+                        right: 0,
+                        opacity: 1,
+                      },
+                    },
+                    "&:after": {
+                      content: '""',
+                      position: "absolute",
+                      height: "80%",
+                      display: "block",
+                      left: 0,
+                      width: "1px",
+                      bgcolor: "divider",
+                    },
+                  }}
+                >
+                  <Settings />
+                  <ArrowRight
+                    sx={{ position: "absolute", right: 4, opacity: 0 }}
+                  />
+                </IconButton>
+              </Tooltip>
+            </ListItem>
+            <Divider />
+            <Box
+              sx={{
+                bgcolor: open ? "rgba(71, 98, 130, 0.2)" : null,
+                pb: open ? 2 : 0,
+                p: "10px",  alignItems: "center",
+              }}
+            >
+              <Paper
+                component="form"
+                sx={{
+                  p: "2px 4px",
+                  display: "flex",
+                  alignItems: "center",
+                  width: "80%",
+                  margin: '0 auto'
+                }}
+              >
+                <InputBase
+                  sx={{ ml: 1, flex: 1 }}
+                  placeholder="Write acrostic Result to find last key :D"
+                  inputProps={{ "aria-label": "acrostic" }}
+                />
+                <IconButton
+                  type="button"
+                  sx={{ p: "10px" }}
+                  aria-label="search"
+                >
+                  <SearchIcon />
+                </IconButton>
+              </Paper>
+            </Box>
+          </FireNav>
+        </Paper>
+      </ThemeProvider>
+    </Box>
+  );
+}
