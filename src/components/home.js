43a9c1ed432ecc62349b4639524befd99b63bb01
Andrés Londoño <andreslon@outlook.com>
2022-08-26 01:54:51 -0500

    final challenge

--- src/components/home.js
+++ src/components/home.js
@@ -21,6 +21,7 @@ import PermMedia from "@mui/icons-material/PermMedia";
 import Dns from "@mui/icons-material/Dns";
 import InputBase from "@mui/material/InputBase";
 import SearchIcon from "@mui/icons-material/Search";
+import Alert from "@mui/material/Alert";
 
 import Public from "@mui/icons-material/Public";
 import background from "./../bg.png";
@@ -47,78 +48,96 @@ const FireNav = styled(List)({
 });
 
 export default function CustomizedList() {
-  const [open, setOpen] = React.useState(true);
-
+  const [error, setError] = React.useState(false);
+  const [code, setCode] = React.useState("");
+  const handleChange = (event) => {
+    setCode(event.target.value);
+  };
   return (
-    <Box sx={{ display: "flex" }}>
-      <ThemeProvider
-        theme={createTheme({
-          components: {
-            MuiListItemButton: {
-              defaultProps: {
-                disableTouchRipple: true,
-              },
-            },
-          },
-          palette: {},
-        })}
-      >
-        <Paper elevation={0} sx={{ width: "80%" }}>
-          <FireNav component="nav" disablePadding>
-            <img src={background} className="App-bg" alt="Legendary Player" />
-            <Divider />
-            <Box
-              sx={{
-                pb: open ? 2 : 0,
-                p: "10px",
-                alignItems: "center",
-              }}
-            >
-              <Typography gutterBottom variant="h5" component="div">
-                Legendary Player
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                To obtain the third key you must enter the key that you have
-                solved in the acrostic and we will redirect you to the final
-                form.
-              </Typography>
-            </Box>
+    <Box>
+      <Paper sx={{ width: "100%" }}>
+        <FireNav component="nav" disablePadding>
+          <img src={background} className="App-bg" alt="Legendary Player" />
+          <Divider />
+          <Box
+            sx={{
+              pb: 2,
+              p: "10px",
+              alignItems: "center",
+            }}
+          >
+            <Typography gutterBottom variant="h5" component="div">
+              Legendary Player
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              To obtain the third key you must enter the key that you have
+              solved in the acrostic and we will redirect you to the final form.
+            </Typography>
+          </Box>
 
-            <Divider />
-            <Box
+          <Divider />
+
+          <Box
+            sx={{
+              bgcolor: "rgba(71, 98, 130, 0.2)",
+              pb: 2,
+              p: "10px",
+              alignItems: "center",
+            }}
+          >
+            <Paper
+              component="form"
               sx={{
-                bgcolor: open ? "rgba(71, 98, 130, 0.2)" : null,
-                pb: open ? 2 : 0,
-                p: "10px",
+                p: "2px 4px",
+                display: "flex",
                 alignItems: "center",
+                width: "80%",
+                margin: "0 auto",
               }}
+              noValidate
+              autoComplete="off"
             >
-              <Paper
-                component="form"
-                sx={{
-                  p: "2px 4px",
-                  display: "flex",
-                  alignItems: "center",
-                  width: "80%",
-                  margin: "0 auto",
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Write acrostic result to find the last key"
+                inputProps={{ "aria-label": "acrostic" }}
+                value={code}
+                onChange={handleChange}
+              />
+              <IconButton
+                onClick={() => {
+                  if (!code) {
+                    setError(true);
+                  } else {
+                    let c =
+                      "01001001 01011001 01001100 01011010 01010100 01010000 01000101";
+
+                    let strVal = c
+                      .split(" ")
+                      .map((l) => String.fromCharCode(parseInt(l, 2)))
+                      .join("");
+
+                    if (strVal == code) {
+                      window.location.replace(
+                        "https://forms.gle/fqkrF1uxHogH1Mw58"
+                      );
+                      setError(false);
+                    } else {
+                      setError(true);
+                    }
+                  }
                 }}
+                type="button"
+                sx={{ p: "10px" }}
+                aria-label="search"
               >
-                <InputBase
-                  sx={{ ml: 1, flex: 1 }}
-                  placeholder="Write acrostic result to find the last key"
-                  inputProps={{ "aria-label": "acrostic" }}
-                />
-                <IconButton
-                  type="button"
-                  sx={{ p: "10px" }}
-                  aria-label="search" >
-                  <SearchIcon />
-                </IconButton>
-              </Paper>
-            </Box>
-          </FireNav>
-        </Paper>
-      </ThemeProvider>
+                <SearchIcon />
+              </IconButton>
+            </Paper>
+          </Box>
+          {error && <Alert severity="error">Invalid acrostic result</Alert>}
+        </FireNav>
+      </Paper>
     </Box>
   );
 }
